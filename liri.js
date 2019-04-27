7270e46e26a0e43b303e89de0c70eded92787a9f
KAB <47466350+KwasiBell@users.noreply.github.com>
2019-04-27 18:02:14 -0400

    working spotify and omdb

--- liri.js
+++ liri.js
@@ -1,36 +1,105 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
+var axios = require("axios");
+var spotifyKeys = require("./keys.js")
+var Spotify = require("node-spotify-api");
 
-var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(spotifyKeys.spotify);
 
-function spotifyThisSong() {
-  console.log(`\n - - - - -\n\nSEARCHING FOR..."${userQuery}"`);
+const [node, file, ...args] = process.argv;
+
+// if (args[0] === "movie-this") {
+//   if (args[0] === undefined) {
+//     getMovie("Mr. +Nobody");
+//   }
+//   else {
+//     getMovie(args.slice(1).join("+"));
+//   }
+// };
+
+// if (args[0] === "spotify-this-song") {
+//   if (args[1] === undefined) {
+//     spotifySong("See you again");
+//   }
+//   else {
+//     var songTitle = args.slice(1).join("");
+//     spotifySong(songTitle);
+//   }
+// };
+
+if (args[0] === "do-what-it-says") {
+  fs.readFile("random.txt", "utf8", function (error, data) {
     if (error) {
-      return console.log('Error occurred: ' + error);
-    }
-      let spotifyArr = data.tracks.items;
-
-      for (i = 0; i < spotifyArr.length; i++) {
-        console.log(`DOPE TUNES! \n\nArtist: ${data.tracks.items[i].album.artist[0].name} \nSong: ${data.tracks.items[i].name}\nSpotify Link: ${data.tracks.items[i].external_urls.spotify}\nAlbum: ${data.tracks.items.[i].album.name}\n\n - - - - -`)
-
-
-      };
-
-
-
-
-
+      return console.log(error);
+    };
+  });
 }
 
-spotify.search({ type: 'track', query: userQuery, limit: 1}, function (error, data){} )
 
 
+var command = process.argv[2];
+if (command === "movie-this") {
+  if (process.argv[3] === undefined) {
+    getMovie("Mr. Nobody")
+  }
+  else {
+    getMovie(process.argv.slice(3).join("+"))
+  }
+} else if (command  === "spotify-this-song") {
+  if (process.argv[3] === undefined) {
+    spotifySong("See you again")
+  }
+  else {
+    spotifySong(process.argv.slice(3).join(" "))
+  }
+} else if (command  === "concert-this") {
+    if (process.argv[3] === undefined) {
+      concertThis("Wu Tang")
+    }
+    else {
+      concertThis(process.argv.slice(3).join(" "))
+    }
+}
 
-function concertThis() {};
-("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+
+    function concertThis(band) {
+  var queryURL = "https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp"
+}
 
 
+    function spotifySong(songName) {
+      spotify.search({ type: 'track', query: songName, limit: 5 }, function (err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
 
+        data.tracks.items.forEach(function (element) {
+          console.log("------");
 
-function movieThis() {};
+          console.log(`Artist: ${element.artists[0].name}`);
+          console.log(`Song: ${songName}`);
+          console.log(`Spotify Preview Link: ${element.preview_url}`);
+          console.log(`Album: ${element.album.name}`);
+        });
+      });
+    }
+
+      function getMovie(movieName) {
+        console.log(movieName)
+        axios
+          .get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy")
+          .then(function (movie) {
+            console.log("");
+            console.log(`Title: ${movie.data.Title}`);
+            console.log(`Released: ${movie.data.Year}`);
+            console.log(`IMDB Rating: ${movie.data.Ratings[0].Value}`);
+            console.log(`Rotten Tomatoes Rating: ${movie.data.Ratings[0].Value}`);
+            console.log(`Produced in: ${movie.data.Country}`);
+            console.log(`Plot: ${movie.data.Plot}`);
+            console.log(`Starring: ${movie.data.Actors}`);
+
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+      };
